b459b4be51574705c955d650187a25a6fc57796c
victorfernandesraton <vfbraton@gmail.com>
2023-02-03 02:37:12 -0300

    fix: webpack identation

--- webpack.config.js
+++ webpack.config.js
@@ -3,28 +3,28 @@ const [server, client] = require('nullstack/webpack.config')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 function customClient(...args) {
-  const config = client(...args)
-  const rule = config.module.rules.find((r) => r.test.test('.css'))
-  rule.use.push({
-    loader: require.resolve('postcss-loader'),
-    options: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-        },
-      },
-    },
-  })
-  if (config.mode === 'buildtest') {
-    config.plugins.push(
-      new BundleAnalyzerPlugin({
-        analyzerMode: 'server',
-        logLevel: 'info',
-      }),
-    )
-  }
+	const config = client(...args)
+	const rule = config.module.rules.find((r) => r.test.test('.css'))
+	rule.use.push({
+		loader: require.resolve('postcss-loader'),
+		options: {
+			postcssOptions: {
+				plugins: {
+					tailwindcss: {},
+				},
+			},
+		},
+	})
+	if (config.mode === 'buildtest') {
+		config.plugins.push(
+			new BundleAnalyzerPlugin({
+				analyzerMode: 'server',
+				logLevel: 'info',
+			}),
+		)
+	}
 
-  return config
+	return config
 }
 
 module.exports = [server, customClient]
